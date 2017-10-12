36813ccb52706e48bd9e8a04cbb659caa0174a86
w0178837 <N4s6c4c2>
2017-10-12 11:19:29 -0300

    finished class, the char example doesn't work

--- main.cpp
+++ main.cpp
@@ -1,7 +1,48 @@
 #define _SCL_SECURE_NO_WARNINGS // ignore "false" errors
 #include <iostream>
+#include <stdio.h>
+#include <string.h>
+
+void strtokExample();
+void chartoExample(const string &strIn);
+
+using namespace std;
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    string s = "Hello $";
+    cout << "Begin string token demo " << endl;
+    strtokExample();
+    cout << "Begin char token demo" << endl;
+    chartoExample(s);
     return 0;
 }
+
+void strtokExample(){
+    int numT = 0; // token counter
+    char str[] = "hello,world"; // string to be tokenized
+    char *pch = NULL;
+    printf("Tokenizing string \"%s\" into tokens:\n\n", str);
+    pch = strtok(str," ,.-");
+    while(pch!=NULL)
+    {
+        printf("%s\n",pch);
+        pch=strtok(NULL," ,.-");
+        numT++;
+    }
+    cout << "\nTotal tokens: \n" << numT << endl;
+    cout << "Press any key for next example\n" << endl;
+    cin.ignore();
+}
+
+
+void chartoExample(const string &strIn) {
+    char dol;
+    cout << "Parsing string: ""Hello $"" into tokens: \n" << endl;
+    char cArray[20]; // create char array size 20
+    strIn.copy(cArray,20); // copy string into char array
+    for (char i : cArray) {
+        if(i =='$')
+            dol = i;
+    }
+}
+// end chartoExample
